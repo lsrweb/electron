1c4f4481e70c33be75f562e97c7ecf83da79a684
KuCaiTx <siriforever.ltd@gmail.com>
2024-08-19 11:12:02 +0800

    feat: Add sass dependency and update styles in App.vue

--- vite/src/render_/App.vue
+++ vite/src/render_/App.vue
@@ -1,7 +1,13 @@
 <template>
-    <h1 class="">💖 Hello 1!</h1>
+    <h1 class="text-red">💖 Hello 1!</h1>
     <p>Welcome to your Electron application.</p>
   </template>
   
   <script setup>
   </script>
+
+  <style lang="scss" scoped>
+h1 {
+  @apply text-4xl
+}
+</style>
