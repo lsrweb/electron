8cdca89168a48bbf3f72eb8811b75b0b26401833
KuCaiTx <siriforever.ltd@gmail.com>
2024-08-19 17:44:54 +0800

    chore: Update TypeScript module and module resolut ion settings

--- vite/src/render_/App.vue
+++ vite/src/render_/App.vue
@@ -1,18 +1,15 @@
 <template>
-  <div>
-    <h1 class="text-red">💖 Hello 1!</h1>
-    <p>Welcome to your Electron application.</p>
-    <el-button>123</el-button>
-    {{ok}}
-  </div>
+  <el-config-provider>
+    <RouterView />
+  </el-config-provider>
 </template>
 
 <script setup>
-const ok = ref(1)
+const ok = ref(1);
 </script>
 
 <style lang="scss" scoped>
 h1 {
-  @apply text-4xl
+  @apply text-4xl;
 }
 </style>
