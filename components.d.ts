1fe06044843940de735526864bf0c782adc87c02
kucait <siriforever.ltd@gmail.com>
2024-08-19 22:39:53 +0800

    refactor: Update HTML title and add Content-Security-Policy meta tag

--- components.d.ts
+++ components.d.ts
@@ -7,7 +7,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    ElButton: typeof import('element-plus/es')['ElButton']
     ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
