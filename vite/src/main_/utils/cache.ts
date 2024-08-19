4c1b1b95e96a76cf0a6c632e4a07e3158a184578
KuCaiTx <siriforever.ltd@gmail.com>
2024-08-19 10:48:02 +0800

    refactor: 更新PostCSS配置文件和Tailwind CSS配置
    这个提交对PostCSS配置文件和Tailwind CSS配置进行了更新。

---
+++ vite/src/main_/utils/cache.ts
@@ -0,0 +1,90 @@
+import * as fs from "fs";
+import * as path from "path";
+import { app, dialog } from "electron";
+
+interface FileStoreOptions {
+  cacheDir?: string;
+  versioned?: boolean;
+  version?: string;
+}
+
+class FileStore {
+  private cacheFilePath: string;
+
+  constructor(options: FileStoreOptions = {}) {
+    const {
+      cacheDir = app.getPath("userData"),
+      versioned = false,
+      version = "",
+    } = options;
+    const fileName =
+      versioned && version ? `cache_${version}.json` : "cache.json";
+    this.cacheFilePath = path.join(cacheDir, fileName);
+
+    if (!fs.existsSync(this.cacheFilePath)) {
+      fs.writeFileSync(this.cacheFilePath, JSON.stringify({}));
+    }
+
+    console.log(`Cache file path: ${this.cacheFilePath}`);
+  }
+
+  private readCache(): Record<string, any> {
+    const data = fs.readFileSync(this.cacheFilePath, "utf-8");
+    return JSON.parse(data);
+  }
+
+  private writeCache(data: Record<string, any>): void {
+    fs.writeFileSync(this.cacheFilePath, JSON.stringify(data, null, 2));
+  }
+
+  public setCache(key: string, value: any): void {
+    const cache = this.readCache();
+    cache[key] = value;
+    this.writeCache(cache);
+  }
+
+  public getCache(key: string): any {
+    const cache = this.readCache();
+    return cache[key];
+  }
+
+  public clearCache(key: string): void {
+    const cache = this.readCache();
+    delete cache[key];
+    this.writeCache(cache);
+  }
+
+  public hasCache(key: string): boolean {
+    const cache = this.readCache();
+    return Object.prototype.hasOwnProperty.call(cache, key);
+  }
+
+  public clearAllCache(): void {
+    this.writeCache({});
+  }
+
+  public exportConfig(): void {
+    const cache = this.readCache();
+    const filePath = dialog.showSaveDialogSync({
+      title: "保存配置文件",
+      defaultPath: path.join(app.getPath("documents"), "config.json"),
+      filters: [{ name: "JSON 文件", extensions: ["json"] }],
+    });
+
+    if (filePath) {
+      fs.writeFileSync(filePath, JSON.stringify(cache, null, 2));
+    }
+  }
+
+  public importConfig(filePath: string): void {
+    if (fs.existsSync(filePath)) {
+      const data = fs.readFileSync(filePath, "utf-8");
+      const importedCache = JSON.parse(data);
+      this.writeCache(importedCache);
+    } else {
+      throw new Error("文件不存在");
+    }
+  }
+}
+
+export default FileStore;
--- vite/src/renderer.ts
+++ vite/src/renderer.ts
@@ -1,4 +1,6 @@
 import { createApp } from "vue";
 import App from "./render_/App.vue";
 
+import "./index.css";
+
 createApp(App).mount("#app");
