08b4d43b0d174841a6a49eb2699bcc08526d0bf7
kucait <siriforever.ltd@gmail.com>
2024-09-05 00:20:58 +0800

    Refactor fileReplace.ts to improve code structure and error handling

---
+++ src/main_/controller/core/fileReplace.ts
@@ -0,0 +1,9 @@
+import { copyFileSync } from "fs-extra";
+
+export function fileReplace(originalFilePath: string, tempFilePath: string, replaceFilePath: string) {
+  try {
+    //
+  } catch (error) {
+    console.error(error);
+  }
+}
