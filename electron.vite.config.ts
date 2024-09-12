212a14bdcfdd3d71c32500d781e601fff0bb7191
KuCaiTx <siriforever.ltd@gmail.com>
2024-09-12 18:26:52 +0800

    Refactor postcss.config.mjs, tsconfig.web.json, and tailwind.config.ts

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,20 +1,46 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
+import stylePlugin from 'esbuild-style-plugin'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
-export default defineConfig({
-  main: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  preload: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  renderer: {
-    resolve: {
-      alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
+export default defineConfig(({ mode }) => {
+  const is_prod = mode === 'production'
+
+  return {
+    main: {
+      clean: true,
+      plugins: [externalizeDepsPlugin()],
+      format: 'esm',
+      // The requested module 'electron-updater' is a CommonJS module, which may not support all module.exports as named exports.
+      noExternal: is_prod ? [/(.*)/] : ['electron-updater'],
+      minify: is_prod
     },
-    plugins: [vue()]
+    preload: {
+      clean: true,
+      plugins: [externalizeDepsPlugin()],
+      format: 'cjs',
+      minify: is_prod
+    },
+    renderer: {
+      resolve: {
+        alias: {
+          '@renderer': resolve('src/renderer/src')
+        }
+      },
+      plugins: [vue()],
+      build: {
+        clean: true,
+        minify: is_prod,
+        esbuildPlugins: [
+          stylePlugin({
+            postcss: {
+              plugins: [tailwindcss(), autoprefixer()]
+            }
+          })
+        ]
+      }
+    }
   }
 })
--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -5,7 +5,7 @@ const ipcHandle = () => window.electron.ipcRenderer.send('ping')
 </script>
 
 <template>
-  <img alt="logo" class="logo" src="./assets/electron.svg" />
+  <img alt="logo" class="logo w-72" src="./assets/electron.svg" />
   <div class="creator">Powered by electron-vite</div>
   <div class="text">
     Build an Electron app with
