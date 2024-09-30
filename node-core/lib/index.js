8c0c091baa05b90b13f9d3e0c35279c47621f436
KuCaiLSR <siriforever.ltd@gmail.com>
2024-09-30 23:44:45 +0800

    chore: Update unihelper-core to version 1.1.21 in package.json

--- node-core/lib/index.js
+++ node-core/lib/index.js
@@ -1,7 +1,3 @@
 const unihelper = require('../build/Release/unihelper-core.node');
-console.log(unihelper.execShell(
-  'node -v'
-) === 'v22.3.0');
-
 module.exports = unihelper;
 exports = unihelper;
